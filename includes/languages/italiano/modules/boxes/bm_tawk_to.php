44c6a31e7c653843047801fdcca35cf07611ec57
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-10-31 19:01:42 +0100

    Add files via upload

---
+++ includes/languages/italiano/modules/boxes/bm_tawk_to.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2017 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_BOXES_TAWK_TO_TITLE', 'tawk.to Chat Widget');
+  define('MODULE_BOXES_TAWK_TO_DESCRIPTION', 'Abilita/set  tawk.to chat widget nella pagina pubblica');
+
+  define('MODULE_BOXES_TAWK_TO_TAWKTO_LINK_TITLE', 'Imposta tawk.to widget');
+
+  define('MODULE_BOXES_TAWK_TO_NAVBAR_TITLE_1', 'Il mio Profilo');
+  define('MODULE_BOXES_TAWK_TO_NAVBAR_TITLE_2', 'Imposta chat widget');
+
+  define('MODULE_BOXES_TAWK_TO_HEADING_TITLE', 'Imposta chat widget');
+
+  define('MODULE_BOXES_TAWK_TO_TAWKTO_TITLE', 'Imposta chat widget');
+
+  define('MODULE_BOXES_TAWK_TO_SUCCESS_CHAT_SET', 'La tawk.to widget è stata correttamente impostata.');
